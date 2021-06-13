50a31f496ec8e4a8865cacc40d9e2928eb607822
Martin Eršek <nitram147@gmail.com>
2021-06-13 22:42:27 +0200

    Fixed tabs to space in smartcontract code

--- src/smartcontracts/sigma_stamp_nft.scala
+++ src/smartcontracts/sigma_stamp_nft.scala
@@ -1,35 +1,35 @@
 {
 
-	val sigmaStampNftIssuanceOK = {
-		
-		val assetType = OUTPUTS(0).R7[Coll[Byte]].get
-		val stampedDocHash = OUTPUTS(0).R8[Coll[Byte]].get
-		val issued = OUTPUTS(0).tokens.getOrElse(0, (INPUTS(0).id, 0L))
+    val sigmaStampNftIssuanceOK = {
+        
+        val assetType = OUTPUTS(0).R7[Coll[Byte]].get
+        val stampedDocHash = OUTPUTS(0).R8[Coll[Byte]].get
+        val issued = OUTPUTS(0).tokens.getOrElse(0, (INPUTS(0).id, 0L))
 
-		INPUTS(0).id == issued._1 && issued._2 == 1 &&
-		OUTPUTS(0).value == $ergsSendTogetherWithNFTL &&
+        INPUTS(0).id == issued._1 && issued._2 == 1 &&
+        OUTPUTS(0).value == $ergsSendTogetherWithNFTL &&
 
-		OUTPUTS(0).propositionBytes == PK("$userAddress").propBytes &&
-		OUTPUTS(1).value == $ergsFeeForSigmaStampServiceL &&
+        OUTPUTS(0).propositionBytes == PK("$userAddress").propBytes &&
+        OUTPUTS(1).value == $ergsFeeForSigmaStampServiceL &&
 
-		OUTPUTS(1).propositionBytes == PK("$sigmaStampProviderAddress").propBytes &&
-		assetType == fromBase64($assetTypeValue) &&
-		stampedDocHash == fromBase64($documentHash) &&
-		OUTPUTS.size == 3
+        OUTPUTS(1).propositionBytes == PK("$sigmaStampProviderAddress").propBytes &&
+        assetType == fromBase64($assetTypeValue) &&
+        stampedDocHash == fromBase64($documentHash) &&
+        OUTPUTS.size == 3
 
-	}
+    }
 
-	val returnFunds = {
+    val returnFunds = {
 
-		val total_without_fee = INPUTS.fold(0L, {(x:Long, b:Box) => x + b.value}) - $returnTransactionFee
+        val total_without_fee = INPUTS.fold(0L, {(x:Long, b:Box) => x + b.value}) - $returnTransactionFee
 
-		OUTPUTS(0).value >= total_without_fee &&
-		OUTPUTS(0).propositionBytes == PK("$userAddress").propBytes &&
-		(PK("$sigmaStampAssemblerNodeAddr") || HEIGHT > $refundHeightThreshold) &&
-		OUTPUTS.size == 2
+        OUTPUTS(0).value >= total_without_fee &&
+        OUTPUTS(0).propositionBytes == PK("$userAddress").propBytes &&
+        (PK("$sigmaStampAssemblerNodeAddr") || HEIGHT > $refundHeightThreshold) &&
+        OUTPUTS.size == 2
 
-	}
+    }
 
-	sigmaProp(sigmaStampNftIssuanceOK || returnFunds)
+    sigmaProp(sigmaStampNftIssuanceOK || returnFunds)
 
 }
