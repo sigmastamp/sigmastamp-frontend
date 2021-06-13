26c6a8dcf318d6a3a8baf6ab5f0462d37a69e696
Pavol Hejný <me@pavolhejny.com>
2021-06-13 21:25:01 +0000

    createSigmaStampNft

--- src/smartcontracts/createSigmaStampNft.ts
+++ src/smartcontracts/createSigmaStampNft.ts
@@ -1,20 +1,24 @@
-import { getCurrentBlockchainHeight } from "./getCurrentBlockchainHeight";
-import { isUserAddressCorrect } from "./isUserAddressCorrect";
+import { getCurrentBlockchainHeight } from './getCurrentBlockchainHeight';
+import { isUserAddressCorrect } from './isUserAddressCorrect';
 
 interface ICreateSigmaStampNft {
-    /* base64 */
-    documentHash: string;
-
+    documentHashInBase64: string;
+    documentHashInHex: string;
     userAddress: string;
 }
 
-export async function createSigmaStampNft({ userAddress, documentHash }: ICreateSigmaStampNft): Promise<string> {
+export async function createSigmaStampNft({
+    userAddress,
+    documentHashInBase64,
+    documentHashInHex,
+}: ICreateSigmaStampNft): Promise<{ amount: number; address: string }> {
     if (!isUserAddressCorrect(userAddress)) {
         throw new Error(`User address "${userAddress}" is not correct.`);
     }
 
-    const ergsSendTogetherWithNFT = 0.1; /* TODO: User settable */
-    const ergsFeeForSigmaStampService = 0.1; /* Our fee */
+    const mintingFee = 20000000; /* TODO: User settable */
+    const ergsSendTogetherWithNFT = 100000000; /* TODO: User settable */
+    const ergsFeeForSigmaStampService = 100000000; /* Our fee */
     const sigmaStampProviderAddress = '3Ww7y6vi4NhFZ1ufsEF8vQNyGrvhNmeMmDWP9h3s4qSEFSMoGooV';
     const assetTypeValue = 'Ad4=';
     const returnTransactionFee = 10000000;
@@ -38,7 +42,7 @@ export async function createSigmaStampNft({ userAddress, documentHash }: ICreate
     
             OUTPUTS(1).propositionBytes == PK("${sigmaStampProviderAddress}").propBytes &&
             assetType == fromBase64(${assetTypeValue}) &&
-            stampedDocHash == fromBase64(${documentHash}) &&
+            stampedDocHash == fromBase64(${documentHashInBase64}) &&
             OUTPUTS.size == 3
     
         }
@@ -58,4 +62,65 @@ export async function createSigmaStampNft({ userAddress, documentHash }: ICreate
     
     }
     `;
+
+    const compilerResponse = await fetch(`http://assembler.sigmastamp.ml:14747/compile`, {
+        method: 'POST',
+        body: sourceInScala,
+    });
+    const compilerResponseBody = await compilerResponse.json();
+
+    const compiledSmartContractAddress = compilerResponseBody.address;
+    const ergoAmountRequired = ergsSendTogetherWithNFT + ergsFeeForSigmaStampService + mintingFee;
+    const documentHashInErgoFormat = `e20${documentHashInHex}`;
+    const verifyLinkInErgoFormat = `0e61${
+        /* !!! Convert to hex */ `http://sigmastamp.ml/verify?hash=a16d5705c031866f5c5dd1ba39e43538193b45718af5a50a115e1c8d67c209cd`
+    }`;
+
+    const requestBody = {
+        address: compiledSmartContractAddress,
+        returnTo: userAddress,
+        startWhen: { erg: ergoAmountRequired },
+        txSpec: {
+            requests: [
+                {
+                    ergValue: ergsSendTogetherWithNFT,
+                    address: userAddress,
+                    name: 'SigmaStampNFT',
+                    amount: 1,
+                    decimals: 0,
+                    description:
+                        'Proof of existence of document with Blake2b-256 hash specified in R8 register during minting of this NFT token.',
+                    registers: { R7: '0e0201de', R8: documentHashInErgoFormat, R9: verifyLinkInErgoFormat },
+                },
+                { value: ergsFeeForSigmaStampService, address: sigmaStampProviderAddress },
+            ],
+            fee: mintingFee,
+            inputs: ['$userIns'],
+            dataInputs: [],
+        },
+    };
+
+    const followResponse = await fetch(`http://assembler.sigmastamp.ml:14747/follow`, {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+    });
+
+    const followResponseBody = await followResponse.json();
+    const { id, dueTime } = followResponseBody;
+
+    {
+        // Loop
+        const watchResponse = fetch(`http://assembler.sigmastamp.ml:14747/result/id`);
+        const watchResponseBody = await followResponse.json();
+        const { id, tx, detail /* pending, returning, mined, success, timeout, returnFailed */ } = followResponseBody;
+
+        if (detail === 'success') {
+            // !!! And now take tx and create big certificate
+        }
+    }
+
+    return {
+        amount: ergoAmountRequired / 1000000000,
+        address: compilerResponseBody.address,
+    };
 }
