7d88f0939afa84054ec16e5a54f7d9630c7ac555
Pavol Hejny <me@pavolhejny.com>
2021-09-26 15:11:58 +0200

    Config vars are used in the app

--- src/config.ts
+++ src/config.ts
@@ -5,11 +5,16 @@ import { version } from '../package.json';
 
 export const VERSION = version;
 
+console.log(process.env);
+console.log(process.env.ERGO_ASSEMBLER_URL);
 const config = ConfigChecker.from(process.env);
 
-// TODO: !!! check running on native port
-// TODO: !!! check HTTPs
 export const ERGO_ASSEMBLER_URL = config
-    .get('ERGO_ASSEMBLER_URL')
+    .get(
+        'REACT_APP_ERGO_ASSEMBLER_URL' /* TODO: configchecker can ignore prefxes in ConfigChecker.from */,
+    )
     .url()
+    // TODO: .checkHttps()
+    // TODO: .checkNativePort()
+    // TODO: universal .check() in configchecker
     .required().value!;
