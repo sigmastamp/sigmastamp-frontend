0fb3b9299068ad542e756694ceb0aa0ec7bff5e6
Pavol Hejny <me@pavolhejny.com>
2021-09-26 15:00:28 +0200

    Linked payment gate + QR code

--- src/components/PaymentGate.tsx
+++ src/components/PaymentGate.tsx
@@ -15,12 +15,20 @@ export function PaymentGate({
     dueTime /*, getStatus*/,
 }: IPaymentGateProps) {
     // TODO: !!! Deal up in design in superlong addreses like UegztxGAXchyKXtaZYFkGWxDnCpMD329qSNk4YVrc8wrwitM58WVocMT59rPSasNZJwezshytQ74pq9JF8uUGGYHJW64hbooxY54dDCRZnFVqFvXGcj1jUhkXpuYuVaDY1b6LcXq9zRCZCiXZ36Gswg2TqcgZRE1B5ZxUrKSd19XykEfsgx5eWW3k7MDiLxvisPqMxqpZtP8UHvgwucJxZ4Bg86xWC3v4kXVC6o9mkg7z64MqiQy1FrqHvY65mr7UgG1vK8q37mDcgZPkDc7BKmsKSLynXgDNsqT8yirvxmsCU9o5wMsF8BoFPxUnjPGLAp7yBwnqtvXowq3o4pYnc4h93irPEHqbTySBgLSEwFzgRgbVrXtE8FaL1q2LVyg7rbHJHzvjVfJMyQDSGQwecG6iWybNUv9sP7SKanHegS4wn8xKaBGrSmqTNk4aPpRXvGTr6rikYxmTYDmQru6tJSebRUf7mhsbtp3xTngEgFtPVCMQTnuuNtszbtHnMHGxfPntz8gEqfKFjHrM1LSCkWxuiYx92Nj7EHFY5vnM7
+
+    const link = new URL(
+        `https://explorer.ergoplatform.com/payment-request?address=${address}&amount=${amount}`,
+    );
+
+    // TODO: !!! Countdown and show minutes
     return (
         <PaymentGateDiv>
-            <QRCode
-                text={`https://explorer.ergoplatform.com/payment-request?address=${address}&amount=${amount}`}
-            />
-            Pay {amount} ERG to address {address} in ${dueTime} seconds.
+            <QRCode {...{ link }} />
+            Pay {amount} ERG to address{' '}
+            <a href={link.href} target="_blank" rel="noopener noreferrer">
+                {address}
+            </a>
+            in {dueTime} seconds.
             {/*<button
                 onClick={async () => {
                     const status = await getStatus();
--- src/components/QRCode.tsx
+++ src/components/QRCode.tsx
@@ -2,29 +2,38 @@ import QRCodeGenerator, { QRCodeRenderersOptions } from 'qrcode';
 import * as React from 'react';
 
 interface IQRCodeProps extends QRCodeRenderersOptions {
-    text: string;
+    link: URL;
 }
 
 export function QRCode(props: IQRCodeProps) {
+    const href = props.link.href;
+
     return (
-        <canvas
-            ref={(canvasElement) => {
-                if (!canvasElement) {
-                    return;
-                }
-
-                // Note: Making some manipulation (probbably) due to internal errors of qrcode library
-                const options: QRCodeRenderersOptions = { ...props };
-                delete (options as any).text;
-                options.color = options.color || {};
-
-                QRCodeGenerator.toCanvas(canvasElement, props.text, options, (error) => {
-                    if (error) {
-                        console.error(error);
+        <a {...{ href }} target="_blank" rel="noopener noreferrer">
+            <canvas
+                ref={(canvasElement) => {
+                    if (!canvasElement) {
+                        return;
                     }
-                    // console.log('success!');
-                });
-            }}
-        />
+
+                    // Note: Making some manipulation (probbably) due to internal errors of qrcode library
+                    const options: QRCodeRenderersOptions = { ...props };
+                    delete (options as any).href;
+                    options.color = options.color || {};
+
+                    QRCodeGenerator.toCanvas(
+                        canvasElement,
+                        href,
+                        options,
+                        (error) => {
+                            if (error) {
+                                console.error(error);
+                            }
+                            // console.log('success!');
+                        },
+                    );
+                }}
+            />
+        </a>
     );
 }
