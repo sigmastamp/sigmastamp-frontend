a6eace4c0eece8196ce69bfaea83af3953559d26
Pavol Hejny <me@pavolhejny.com>
2021-09-26 14:32:30 +0200

    Passing tests

--- src/smartcontracts/createScript.test.ts
+++ src/smartcontracts/createScript.test.ts
@@ -87,16 +87,12 @@ describe('how creating script works', () => {
                                 val issued = OUTPUTS(0).tokens.getOrElse(0, (INPUTS(0).id, 0L))
 
                                 INPUTS(0).id == issued._1 && issued._2 == 1 &&
-
                                 OUTPUTS(0).value == 100000000L &&
                                 OUTPUTS(0).propositionBytes == PK("3Ww7y6vi4NhFZ1ufsEF8vQNyGrvhNmeMmDWP9h3s4qSEFSMoGooV").propBytes &&
-
                                 OUTPUTS(1).value == 100000000L &&
                                 OUTPUTS(1).propositionBytes == PK("3Ww7y6vi4NhFZ1ufsEF8vQNyGrvhNmeMmDWP9h3s4qSEFSMoGooV").propBytes &&
-
-                                assetType == fromBase64(Ad4=) &&
-                                stampedDocHash == fromBase64(bfcd486994cb8bc0a3e618924137ef4a848ebc5aa98e0c1d5661325e0849e432) &&
-                                
+                                assetType == fromBase64("Ad4=") &&
+                                stampedDocHash == fromBase64("bfcd486994cb8bc0a3e618924137ef4a848ebc5aa98e0c1d5661325e0849e432") &&
                                 OUTPUTS.size == 3
 
                             }
@@ -107,9 +103,7 @@ describe('how creating script works', () => {
 
                                 OUTPUTS(0).value >= total_without_fee &&
                                 OUTPUTS(0).propositionBytes == PK("3Ww7y6vi4NhFZ1ufsEF8vQNyGrvhNmeMmDWP9h3s4qSEFSMoGooV").propBytes &&
-
                                 (PK("3Ww7y6vi4NhFZ1ufsEF8vQNyGrvhNmeMmDWP9h3s4qSEFSMoGooV") || HEIGHT > 10) &&
-                                
                                 OUTPUTS.size == 2
 
                             }
