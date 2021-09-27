9f0324022ee995a3141030887ee243d4e4018e59
Pavol Hejny <me@pavolhejny.com>
2021-09-27 13:17:04 +0200

    hexToErgoFormat passing tests

--- src/smartcontracts/ergoFormat/hex/hexToErgoFormat.ts
+++ src/smartcontracts/ergoFormat/hex/hexToErgoFormat.ts
@@ -7,5 +7,5 @@ import { ergo_format, string_hex } from '../../../interfaces/stringTypes';
  */
 export function hexToErgoFormat(hex: string_hex): ergo_format {
     // TODO: !!! Encode hex
-    return `e20${hex}`;
+    return `0e20${hex}`;
 }
