3e73b161cdcc1b43bb1e12817b5b5d067d83aa82
Pavol Hejny <me@pavolhejny.com>
2021-09-27 13:22:25 +0200

    describe how conversion of url to ergo fromat works

--- src/smartcontracts/ergoFormat/url/urlToErgoFormat.test.ts
+++ src/smartcontracts/ergoFormat/url/urlToErgoFormat.test.ts
@@ -2,6 +2,11 @@ import { urlToErgoFormat } from './urlToErgoFormat';
 
 describe('how conversion of url to ergo fromat works', () => {
     it('can convert simple url to ergo fromat for R9', () => {
-        expect(urlToErgoFormat('TODO:!!!')).toBe('');
+        // TODO: !!!
+        expect(urlToErgoFormat('')).toBe('');
+        expect(urlToErgoFormat('')).toBe('');
+        expect(urlToErgoFormat('')).toBe('');
+        expect(urlToErgoFormat('')).toBe('');
+        expect(urlToErgoFormat('')).toBe('');
     });
 });
