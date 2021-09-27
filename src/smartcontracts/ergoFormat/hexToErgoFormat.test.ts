5b07cc756f2a7c0078574e8cdb6c78b2b7c285f0
Pavol Hejny <me@pavolhejny.com>
2021-09-27 13:10:18 +0200

    Tests

--- src/smartcontracts/ergoFormat/hexToErgoFormat.test.ts
+++ src/smartcontracts/ergoFormat/hexToErgoFormat.test.ts
@@ -1,8 +1,13 @@
 import { hexToErgoFormat } from './hexToErgoFormat';
-import { urlToErgoFormat } from './urlToErgoFormat';
 
 describe('how conversion from hex to ergo fromat works', () => {
-    it('can convert hex to ergo fromat', () => {
-        expect(hexToErgoFormat('TODO:!!!')).toBe('');
+    it('can convert hex to ergo fromat for R8', () => {
+        expect(
+            hexToErgoFormat(
+                '4d1a7eb6b84817769808c9a8a15ac240470d21b3b6f20e93795c2e2c6bae92be',
+            ),
+        ).toBe(
+            '0e204d1a7eb6b84817769808c9a8a15ac240470d21b3b6f20e93795c2e2c6bae92be',
+        );
     });
 });
--- src/smartcontracts/ergoFormat/urlToErgoFormat.test.ts
+++ src/smartcontracts/ergoFormat/urlToErgoFormat.test.ts
@@ -1,7 +1,7 @@
 import { urlToErgoFormat } from './urlToErgoFormat';
 
 describe('how conversion of url to ergo fromat works', () => {
-    it('can convert simple url to ergo fromat', () => {
+    it('can convert simple url to ergo fromat for R9', () => {
         expect(urlToErgoFormat('TODO:!!!')).toBe('');
     });
 });
