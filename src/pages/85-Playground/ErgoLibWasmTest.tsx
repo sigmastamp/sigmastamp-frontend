ebd8b0c03861d009df46a1e331ccb4d56d0b4628
Pavol Hejny <me@pavolhejny.com>
2023-02-20 16:00:25 +0100

    Using ergo-lib-wasm-browser and working

--- src/pages/85-Playground/ErgoLibWasmTest.tsx
+++ src/pages/85-Playground/ErgoLibWasmTest.tsx
@@ -5,7 +5,9 @@ export function ErgoLibWasmTest() {
         <button
             onClick={async () => {
                 let wasm = await ergolib;
-                const result = wasm.TokenId.from_str('2b').to_str();
+                const result = wasm.TokenId.from_str(
+                    '65e3b680cb88cb4609a0414757b1586749071cc8a4954de6c914911f63265f68',
+                ).to_str();
 
                 console.log(result);
             }}
