93bd859e98709422083836160676e36e491fbfeb
Pavol Hejny <me@pavolhejny.com>
2023-02-20 15:27:04 +0100

    Using ergo-lib-wasm-browser and probbably working

--- src/pages/85-Playground/ErgoLibWasmTest.tsx
+++ src/pages/85-Playground/ErgoLibWasmTest.tsx
@@ -1,23 +1,13 @@
 let ergolib = import('ergo-lib-wasm-browser');
 
-declare const ergoConnector: {
-    nautilus: {
-        connect(
-            settings: { createErgoObject: boolean } | undefined,
-        ): Promise<boolean>;
-        isConnected(): Promise<boolean>;
-        disconnect(): Promise<boolean>;
-        getContext(): Promise<object>;
-    };
-};
-
 export function ErgoLibWasmTest() {
     return (
         <button
             onClick={async () => {
                 let wasm = await ergolib;
+                const result = wasm.TokenId.from_str('2b').to_str();
 
-                // !!!
+                console.log(result);
             }}
         >
             Connect to Nautilus wallet
