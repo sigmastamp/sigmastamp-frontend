864300b68a02ebf037267ea2a92558df09b38d11
Pavol Hejny <me@pavolhejny.com>
2023-02-20 23:24:23 +0100

    TODOs for @hejny

--- src/pages/85-Playground/ErgoConnectorButton.tsx
+++ src/pages/85-Playground/ErgoConnectorButton.tsx
@@ -1,45 +1,45 @@
-import { IWallet } from "./Playground";
+import { IWallet } from './Playground';
 
 export function ErgoConnectorButton(props: {
-	wallet: IWallet, setWallet: React.Dispatch<React.SetStateAction<IWallet>>
+    wallet: IWallet;
+    setWallet: React.Dispatch<React.SetStateAction<IWallet>>;
 }) {
+    async function handleClick() {
+        if (!props.wallet.connected) {
+            if (!ergoConnector || !ergoConnector.nautilus) {
+                // TODO: @hejny !!!! - this should work
+                // TODO: Link to better more explainable page
+                alert(
+                    `You need to install Nautilus Wallet first\n https://chrome.google.com/webstore/detail/nautilus-wallet/gjlmehlldlphhljhpnlddaodbjjcchai/related`,
+                );
+                return;
+            }
 
-	async function handleClick(){
-		if(!props.wallet.connected){
+            const isConnected = await ergoConnector.nautilus.connect({
+                createErgoObject: false,
+            });
 
-			if (!ergoConnector || !ergoConnector.nautilus) {
-				// TODO: Link to better more explainable page
-				alert(
-					`You need to install Nautilus Wallet first\n https://chrome.google.com/webstore/detail/nautilus-wallet/gjlmehlldlphhljhpnlddaodbjjcchai/related`,
-				);
-				return;
-			}
+            if (isConnected) {
+                const ctx = await ergoConnector.nautilus.getContext();
+                const address = await ctx.get_change_address();
+                const balance = Number(await ctx.get_balance());
+                props.setWallet((prev: IWallet) => ({
+                    ...prev,
+                    connected: true,
+                    address: address,
+                    balance: balance,
+                }));
+            }
+        } else {
+            if (!ergoConnector || !ergoConnector.nautilus) return;
+            await ergoConnector.nautilus.disconnect();
+            props.setWallet((prev: IWallet) => ({ ...prev, connected: false }));
+        }
+    }
 
-			const isConnected = await ergoConnector.nautilus.connect({
-					createErgoObject: false,
-			});
-
-			if (isConnected) {
-				const ctx = await ergoConnector.nautilus.getContext();
-				const address = await ctx.get_change_address();
-				const balance = Number(await ctx.get_balance());
-				props.setWallet(
-					(prev: IWallet) => ({
-						...prev, connected: true, address: address, balance: balance
-					})
-				);
-			}
-	
-		}else{
-			if (!ergoConnector || !ergoConnector.nautilus) return;
-			await ergoConnector.nautilus.disconnect();
-			props.setWallet((prev: IWallet) => ({...prev, connected: false}));
-		}
-	}
-
-	return (
-		<button	onClick={handleClick}>
-			{(props.wallet.connected) ? "Disconnect" : "Connect"}
-		</button>
-	);
+    return (
+        <button onClick={handleClick}>
+            {props.wallet.connected ? 'Disconnect' : 'Connect'}
+        </button>
+    );
 }
--- src/pages/85-Playground/Playground.tsx
+++ src/pages/85-Playground/Playground.tsx
@@ -1,97 +1,104 @@
-import styled from "styled-components";
-import { useState } from "react";
-import { ErgoConnectorButton } from "./ErgoConnectorButton";
-import { ErgoLibWasmTest } from "./ErgoLibWasmTest";
-import { UploadZone } from "../../components/UploadZone";
-import { UploadZoneSigmastampContent } from "../../components/UploadZoneSigmastampContent";
-import { blake2b256 } from "../../hash/blake2b256";
+import { useState } from 'react';
+import styled from 'styled-components';
+import { UploadZone } from '../../components/UploadZone';
+import { UploadZoneSigmastampContent } from '../../components/UploadZoneSigmastampContent';
+import { blake2b256 } from '../../hash/blake2b256';
 import {
-	get_minimum_required_balance, get_minimum_required_balance_ergs, create_transaction,
-	sign_tx, submit_tx
-} from "../../scripts/transactionBuilder";
-
-import { SIGMASTAMP_SERVICE_FEE_AMOUNT } from "../../configs/SigmaStampConfig";
+    create_transaction,
+    get_minimum_required_balance,
+    get_minimum_required_balance_ergs,
+    sign_tx,
+    submit_tx,
+} from '../../scripts/transactionBuilder';
+import { ErgoConnectorButton } from './ErgoConnectorButton';
 
 export interface IWallet {
-	connected: boolean,
-	address: string,
-	balance: number
+    connected: boolean;
+    address: string;
+    balance: number;
 }
 
 const DEFAULT_WALLET_STATE: IWallet = {
-	connected: false,
-	address: "",
-	balance: 0
+    connected: false,
+    address: '',
+    balance: 0,
 };
 
-
 function check_whether_there_is_enough_balance(balance: number): boolean {
-	return (balance > get_minimum_required_balance());
+    return balance > get_minimum_required_balance();
 }
 
 export function PlaygroundPage() {
+    const [wallet, setWallet] = useState<IWallet>(DEFAULT_WALLET_STATE);
+    const [file, setFile] = useState<any>(null);
+    const [sentTXHash, setSentTXHash] = useState<string>('');
 
-	const [wallet, setWallet] = useState<IWallet>(DEFAULT_WALLET_STATE);
-	const [file, setFile] = useState<any>(null);
-	const [sentTXHash, setSentTXHash] = useState<string>("");
+    function stampItButtonHandler() {
+        if (file === null) return;
+        create_transaction(wallet.address, file.hash).then((tx: any) => {
+            sign_tx(tx).then((stx) => {
+                if (stx === null) {
+                    alert('Problem during transaction signing, try again!');
+                    return;
+                }
+                submit_tx(stx).then((txId) => {
+                    if (txId === null) {
+                        alert(
+                            'Submission of transaction into blockchain network failed, try again!',
+                        );
+                        return;
+                    }
+                    setSentTXHash(txId);
+                });
+            });
+        });
+    }
 
-	function stampItButtonHandler() {
-		if (file === null) return;
-		create_transaction(wallet.address, file.hash).then((tx: any) => {
-			sign_tx(tx).then((stx) => {
-				if(stx === null){
-					alert("Problem during transaction signing, try again!");
-					return;
-				}
-				submit_tx(stx).then((txId) => {
-					if(txId === null){
-						alert("Submission of transaction into blockchain network failed, try again!");
-						return;
-					}
-					setSentTXHash(txId);
-				});
-			});
-		});
-	}
+    return (
+        <PlaygroundPageDiv>
+            <h2>Playground</h2>
+            <ErgoConnectorButton wallet={wallet} setWallet={setWallet} />
 
-	return (
-		<PlaygroundPageDiv>
-			<h2>Playground</h2>
-			<ErgoConnectorButton wallet={wallet} setWallet={setWallet} />
-
-			{
-				(!(wallet.connected)) ?
-					<p>Please connect the wallet first</p> :
-					(!check_whether_there_is_enough_balance(wallet.balance)) ?
-						<p>There is not enough balance for stamping process in your wallet. Required amount is at least {get_minimum_required_balance_ergs()} ERGs.</p> :
-						(file === null) ?
-							<UploadZone
-								onFiles={async (files) => {
-									const file = files[0];
-									const hash = await blake2b256(file);
-									setFile({ file: file, hash: hash });
-								}}
-								isClickable
-							>
-								<UploadZoneSigmastampContent>
-									Drop the files you want to timestamp here!
-								</UploadZoneSigmastampContent>
-							</UploadZone> :
-							(sentTXHash === "") ?
-							<div>
-								<p>{file.file.name}</p>
-								<p>{file.hash}</p>
-								Stamp your file and receive SigmaStampNFT to your address: {wallet.address} by pressing following button:
-								<br />
-								<button onClick={stampItButtonHandler}>
-									Stamp it!
-								</button>
-							</div> :
-							<p>Stamping TX with id {sentTXHash} was successfuly sent!</p>
-			}
-
-		</PlaygroundPageDiv>
-	);
+            {!wallet.connected ? (
+                <p>Please connect the wallet first</p>
+            ) : !check_whether_there_is_enough_balance(wallet.balance) ? (
+                <p>
+                    There is not enough balance for stamping process in your
+                    wallet. Required amount is at least{' '}
+                    {get_minimum_required_balance_ergs()} ERGs.
+                </p>
+            ) : file === null ? (
+                <UploadZone
+                    onFiles={async (files) => {
+                        const file = files[0];
+                        const hash = await blake2b256(file);
+                        setFile({ file: file, hash: hash });
+                    }}
+                    isClickable
+                >
+                    <UploadZoneSigmastampContent>
+                        Drop the files you want to timestamp here!
+                    </UploadZoneSigmastampContent>
+                </UploadZone>
+            ) : sentTXHash === '' ? (
+                <div>
+                    <p>{file.file.name}</p>
+                    <p>{file.hash}</p>
+                    Stamp your file and receive SigmaStampNFT to your address:{' '}
+                    {wallet.address} by pressing following button:
+                    <br />
+                    <button onClick={stampItButtonHandler}>Stamp it!</button>
+                </div>
+            ) : (
+                <p>Stamping TX with id {sentTXHash} was successfuly sent!</p>
+            )}
+        </PlaygroundPageDiv>
+    );
 }
 
 const PlaygroundPageDiv = styled.div``;
+
+/**
+ * TODO: @hejny !!!! Here should be some observing of the transaction status ?nothing/unknown -> unconfirmed -> confirmed
+ *       [🍚] @see https://explorer.ergoplatform.com/en/transactions/d06fb7239d92f575f83a7f9ea51e5c055acefd848cec2b41f7b11591030af2b6
+ */
