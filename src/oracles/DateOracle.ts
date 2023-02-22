70b7784369885532ac38b4b07a3a9256015ec628
Martin Eršek <nitram147@gmail.com>
2023-02-22 16:03:47 +0100

    Updated time oracle to use functions from timeUtils module

--- src/oracles/DateOracle.ts
+++ src/oracles/DateOracle.ts
@@ -1,5 +1,5 @@
 import { IOracle } from './_IOracle';
-import { get_time_with_timezone } from "../scripts/timeUtils";
+import { get_time_with_timezone, get_local_date_from_timestamp } from "../scripts/timeUtils";
 
 export class DateOracle implements IOracle {
     public name = 'DATE';
@@ -14,21 +14,15 @@ export class DateOracle implements IOracle {
             {
                 title: "UnixTimestamp",
                 format: "Count of elapsed seconds since January 1st, 1970 at UTC",
-                value: Math.floor(Date.now() / 1000).toString()
+                value: Math.floor(date.getTime() / 1000).toString()
             },
             {
                 title: 'Date',
                 format: 'YYYY-MM-DD',
-                value: `${date.getUTCFullYear()}-${(
-                    '0' + date.getUTCMonth()
-                ).slice(-2)}-${('0' + date.getUTCDate()).slice(-2)}`,
+                value: get_local_date_from_timestamp(Math.floor(date.getTime() / 1000)),
             },
             {
                 title: 'Time',
-                // format: 'HH:MM',
-                // value: `${('0' + date.getUTCHours()).slice(-2)}:${(
-                //     '0' + date.getUTCMinutes()
-                // ).slice(-2)}:${('0' + date.getUTCSeconds()).slice(-2)}`,
                 format: "24 hours format in user local timezone",
                 value: get_time_with_timezone(date)
             },
--- src/scripts/timeUtils.ts
+++ src/scripts/timeUtils.ts
@@ -1,8 +1,19 @@
 export function get_time_with_timezone(date_arg: Date): string {
-    const userTime = date_arg.toLocaleTimeString('en-US', { timeZoneName: 'short', hour12: false });
-    const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
-    const timeZoneOffset = new Date().getTimezoneOffset();
-    const timeZoneAbbr = timeZoneOffset < 0 ? '+' : '-';
-    const timeZoneFormattedOffset = `${timeZoneAbbr}${Math.abs(Math.floor(timeZoneOffset / 60)).toString().padStart(2, '0')}:${Math.abs(timeZoneOffset % 60).toString().padStart(2, '0')}`;
-    return `${userTime} (UTC${timeZoneFormattedOffset} ${timeZoneName})`;
+	const userTime = date_arg.toLocaleTimeString('en-US', { timeZoneName: 'short', hour12: false });
+	const timeZoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
+	const timeZoneOffset = new Date().getTimezoneOffset();
+	const timeZoneAbbr = timeZoneOffset < 0 ? '+' : '-';
+	const timeZoneFormattedOffset = `${timeZoneAbbr}${Math.abs(Math.floor(timeZoneOffset / 60)).toString().padStart(2, '0')}:${Math.abs(timeZoneOffset % 60).toString().padStart(2, '0')}`;
+	return `${userTime} (UTC${timeZoneFormattedOffset} ${timeZoneName})`;
+}
+
+//unixtimestamp in seconds not in miliseconds as is default timestamp in js
+export function get_time_with_timezone_from_timestamp(unixtimestamp: number): string {
+	return get_time_with_timezone(new Date(unixtimestamp * 1000));
+}
+
+//unixtimestamp in seconds not in miliseconds as is default timestamp in js
+export function get_local_date_from_timestamp(unixtimestamp: number): string{
+	const date = new Date(unixtimestamp * 1000);
+	return `${date.getFullYear()}-${('0' + date.getMonth()).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
 }
